c298cda6aa6b6625ba66d2df32bf584a729d04f8
Pranav Vashi <neobuddy89@gmail.com>
2021-07-16 11:38:10 +0530

    Launcher3: Fix Google QSB behavior when app installed/uninstalled
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -73,6 +73,7 @@ import com.android.launcher3.graphics.TintedDrawableSpan;
 import com.android.launcher3.icons.IconProvider;
 import com.android.launcher3.icons.LauncherIcons;
 import com.android.launcher3.icons.ShortcutCachingLogic;
+import com.android.launcher3.lineage.LineageUtils;
 import com.android.launcher3.model.data.ItemInfo;
 import com.android.launcher3.model.data.ItemInfoWithIcon;
 import com.android.launcher3.pm.ShortcutConfigActivityInfo;
@@ -125,6 +126,7 @@ public final class Utilities {
     private static final long WAIT_BEFORE_RESTART = 250;
 
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
+    public static final String KEY_MINUS_ONE = "pref_enable_minus_one";
     public static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
 
     public static final String SLEEP_GESTURE = "pref_sleep_gesture";
@@ -709,9 +711,8 @@ public final class Utilities {
         return prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_EDIT, true);
     }
 
-    public static boolean showQSB(Context context, Launcher launcher) {
-        LauncherAppState appState = LauncherAppState.getInstance(launcher);
-        if (!appState.isSearchAppAvailable()) {
+    public static boolean showQSB(Context context) {
+        if (!LineageUtils.isPackageEnabled(context, SEARCH_PACKAGE)) {
             return false;
         }
         return isQSBEnabled(context);
@@ -720,6 +721,18 @@ public final class Utilities {
     public static boolean isQSBEnabled(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DOCK_SEARCH, false);
+    }
+
+    public static boolean showMinusOne(Context context) {
+        if (!LineageUtils.isPackageEnabled(context, SEARCH_PACKAGE)) {
+            return false;
+        }
+        return isMinusOneEnabled(context);
+    }
+
+    public static boolean isMinusOneEnabled(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_MINUS_ONE, true);
     }
 
     public static boolean useSleepGesture(Context context) {
